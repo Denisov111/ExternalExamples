6c5ab565ec3b57f6c1e5453b1b66b2eb1a6592d8
Maxxx <Maxxx@DESKTOP-B0ISVCC>
2022-10-25 21:28:29 +0700

    Add project files.

---
+++ External/COMObject.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Dynamic;
+using System.Reflection;
+
+namespace External
+{
+    public class COMObject : DynamicObject
+    {
+        private readonly object instance;
+
+        public static COMObject CreateObject(string progID)
+        {
+            return new COMObject(Activator.CreateInstance(Type.GetTypeFromProgID(progID, true)));
+        }
+
+        public COMObject(object instance)
+        {
+            this.instance = instance;
+        }
+
+        public override bool TryGetMember(GetMemberBinder binder, out object result)
+        {
+            result = instance.GetType().InvokeMember(
+                binder.Name,
+                BindingFlags.GetProperty,
+                Type.DefaultBinder,
+                instance,
+                new object[] { }
+            );
+            return true;
+        }
+
+        public override bool TrySetMember(SetMemberBinder binder, object value)
+        {
+            instance.GetType().InvokeMember(
+                binder.Name,
+                BindingFlags.SetProperty,
+                Type.DefaultBinder,
+                instance,
+                new object[] { value }
+            );
+            return true;
+        }
+
+        public override bool TryInvokeMember(InvokeMemberBinder binder, object[] args, out object result)
+        {
+            result = instance.GetType().InvokeMember(
+                binder.Name,
+                BindingFlags.InvokeMethod,
+                Type.DefaultBinder,
+                instance,
+                args
+            );
+            return true;
+        }
+    }
+}
---
+++ External/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace External
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
